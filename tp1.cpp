f6bacbf8d055535747298b2668bdf9a5b7b58f92
lololerigoloo <laurentcorbeil05@gmail.com>
2026-01-14 14:28:37 -0500

    lumiere qui flash quand tu appuis sur le bouton

--- tp1.cpp
+++ tp1.cpp
@@ -2,49 +2,59 @@
  * Nom: compteur 32 bits
  * Copyright (C) 2005 Matthew Khouzam
  * License http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * Description: Ceci est un exemple simple de programme 
+ * Description: Ceci est un exemple simple de programme
  * Version: 1.1
- * 
+ *
  * Version 1.2 par Jérôme Collin, août 2023
- * 
- */
+ *
+*/
 
-
-#include <avr/io.h> 
+#if defined(__has_include)
+  #if __has_include(<avr/io.h>)
+    #include <avr/io.h>
+  #else
+    // Dummy AVR definitions for host compilation
+    #include <cstdint>
+    using uint8_t = std::uint8_t;
+    volatile uint8_t DDRA = 0, DDRB = 0, DDRC = 0, DDRD = 0;
+    volatile uint8_t PORTA = 0, PORTB = 0, PORTC = 0, PORTD = 0;
+  #endif
+#else
+  #include <avr/io.h>
+#endif
 
 int main()
 {
   // Les 4 ports sont en mode sortie
-  DDRA = 0xFF; 
-  DDRB = 0xFF; 
-  DDRC = 0xFF; 
-  DDRD = 0xFF; 
-  
-  // c'est un compteur de 32 bits d'où le type unsigned long
-  unsigned long compteur = 0; 
+  DDRA = 0xFF;
+  DDRB = 0xFF;
+  DDRC = 0xFF;
+  DDRD = 0xFF;
 
-  while (true) 
-  {   
+  // c'est un compteur de 32 bits d'où le type unsigned long
+  unsigned long compteur = 0;
+
+  while (true)
+  {
     // Le port D prend les bits de 0 a 7 du compteur
     // autrement dit, l'octet de poids le plus faible.
-    PORTD = compteur;       
-                         
-    // Le port C prend les bits de 8 à 15 du compteur 
+    PORTD = compteur;
+
+    // Le port C prend les bits de 8 à 15 du compteur
     // en faisant un decallage de 8 bits.
-    PORTC = compteur >> 8;  
+    PORTC = compteur >> 8;
 
     // Le port B prend les bits de 16 à 23 du compteur
     // en faisant un decallage de 16 bits.
-    PORTB = compteur >> 16; 
+    PORTB = compteur >> 16;
 
     // Le port B prend les bits de 24 à 31 du compteur
     // en faisant un decallage de 24 bits.
     PORTA = compteur >> 24;
-    
-    // si le compteur est à 0xFFFF, il revient à 0 naturellement
-    compteur++; 
-  }
-  
-  return 0; 
-}
 
+    // si le compteur est à 0xFFFF, il revient à 0 naturellement
+    compteur++;
+  }
+
+  return 0;
+}
--- tp1_2.cpp
+++ tp1_2.cpp
@@ -2,57 +2,105 @@
  * Nom: compteur 32 bits
  * Copyright (C) 2005 Matthew Khouzam
  * License http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * Description: Ceci est un exemple simple de programme 
+
+ * Description: Ceci est un exemple simple de programme
  * Version: 1.1
- * 
+
+ *
  * Version 1.2 par Jérôme Collin, août 2023
- * 
+
+ *
  */
 
-
-#include <avr/io.h> 
+#include <avr/io.h>
+#define F_CPU 8000000
+#include <util/delay.h>
 
 int main()
 {
   // Les 4 ports sont en mode sortie
-  DDRA = 0xFF; 
-  DDRB = 0xFF; 
-  DDRC = 0xFF; 
-  DDRD = 0xFF; 
-  
+  DDRA = 0xFF;         // 11111111
+  DDRB = 0xFF;         // 11111111
+  DDRC = 0xFF;         // 11111111
+  DDRD = 0xFF;         // 11111111
+  DDRD &= ~(1 << PD2); // 11111011
+
   // c'est un compteur de 32 bits d'où le type unsigned long
-  unsigned long compteur = 0;  
-  const int x = 0x10101010;
-  const int y = 0x01010101;
-  int temp = x;
-  while (true) 
-  {   
+  unsigned long compteur = 0;
+  const int vert = 0b01;
+  const int delai = 500;
+  const int rouge = 0b10;
+  const int eteint = 0b00;
+  // const int bleu = 0b11;
+  // const int x = 0b10100110;
+  // const int xx = 0b10101010;
+  // const int y = 0b01010101;
+  // int temp = xx;
+  PORTA = vert;
+  while (true)
+
+  {
+    if ((PIND & (1 << PD2)) != 0)
+    {
+      
+      _delay_ms(delai);
+      if (PORTA == vert)
+      {
+        PORTA = rouge;
+      }
+      else
+      {
+        PORTA = vert;
+      }
+    }
+    else
+    {
+      PORTA = eteint;
+    }
     // Le port D prend les bits de 0 a 7 du compteur
     // autrement dit, l'octet de poids le plus faible.
-    PORTD = compteur;       
-                         
-    // Le port C prend les bits de 8 à 15 du compteur 
-    // en faisant un decallage de 8 bits.
-    PORTC = compteur >> 8;  
+    // PORTD = compteur;
 
-    // Le port B prend les bits de 16 à 23 du compteur
-    // en faisant un decallage de 16 bits.
-    PORTB = compteur >> 16; 
+    // // Le port C prend les bits de 8 à 15 du compteur
+    // // en faisant un decallage de 8 bits.
+    // PORTC = compteur >> 8;
 
-    // Le port B prend les bits de 24 à 31 du compteur
-    // en faisant un decallage de 24 bits.
-    PORTA = temp;
-    if (temp == x) {
-        temp = y;
-    }
-    else {
-        temp = x;
-    }
-    
-    // si le compteur est à 0xFFFF, il revient à 0 naturellement 
+    // // Le port B prend les bits de 16 à 23 du compteur
+    // // en faisant un decallage de 16 bits.
+    // PORTB = bleu;
+
+    // // Le port B prend les bits de 24 à 31 du compteur
+    // // en faisant un decallage de 24 bits.
+    // if (compteur > 0x0FFF)
+    // {
+    //   PORTA = eteint;
+    // }
+    // else
+    // {
+    //   PORTA = temp;
+    //   if (temp == xx)
+    //   {
+    //     temp = y;
+    //   }
+    //   else
+    //   {
+    //     temp = xx;
+    //   }
+    //   if (temp == xx)
+    //   {
+    //     // attente active
+    //     for (volatile int i = 0; i < 5; i++)
+
+    //   }
+    // }
+
+    // if (compteur > 0x9FFF)
+    // {
+    //   compteur = 0;
+    // }
+    // // si le compteur est à 0xFFFF, il revient à 0 naturellement
     compteur++;
   }
-  
-  return 0; 
-}
 
+  return 0;
+}
