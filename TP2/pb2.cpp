d85010298e5a19cf777ac4f5f96a51b6283fee80
lololerigoloo <laurentcorbeil05@gmail.com>
2026-01-21 20:14:49 -0500

    la version 2 avec les correction du prof

--- TP2/pb2.cpp
+++ TP2/pb2.cpp
@@ -5,58 +5,57 @@
     Description: Programme pour contrôler une lumière à l'aide d'un bouton poussoir avec un état ambre.
 
     Table de vérité de la machine à états :
-    -----------------------------------------------------
-    Etat present   & A &  Etat suivant   & Sortie Z \\
-    Init           & 0 &  Init           & Rouge    \\
-    Init           & 1 &  AppyerAmbre    & Rouge    \\
-    AppuyerAmbre   & 0 &  RelacheVert    & Ambre    \\
-    AppuyerAmbre   & 1 &  AppuyerAmbre   & Ambre    \\
-    RelacherVert   & 0 &  RelacherVert   & Vert     \\
-    RelacherVert   & 1 &  AppuyerRouge   & Vert     \\
-    AppuyerRouge   & 0 &  RelacherEteint & Rouge    \\
-    AppuyerRouge   & 1 &  AppuyerRouge   & Rouge    \\
-    RelacherEteint & 0 &  RelacherEteint & Eteint   \\
-    RelacherEteint & 1 &  AppuyerVert    & Eteint   \\
-    AppuyerVert    & 0 &  Init           & Vert     \\
-    AppuyerVert    & 1 &  AppuyerVert    & Vert     \\
-    -----------------------------------------------------
+    -------------------------------------------------------------
+    Etat present   & Button &  Etat suivant   & Sortie Couleur \\
+    Init           &    0   &  Init           & Rouge          \\
+    Init           &    1   &  AppyerAmbre    & Rouge          \\
+    AppuyerAmbre   &    0   &  RelacheVert    & Ambre          \\
+    AppuyerAmbre   &    1   &  AppuyerAmbre   & Ambre          \\
+    RelacherVert   &    0   &  RelacherVert   & Vert           \\
+    RelacherVert   &    1   &  AppuyerRouge   & Vert           \\
+    AppuyerRouge   &    0   &  RelacherEteint & Rouge          \\
+    AppuyerRouge   &    1   &  AppuyerRouge   & Rouge          \\
+    RelacherEteint &    0   &  RelacherEteint & Eteint         \\
+    RelacherEteint &    1   &  AppuyerVert    & Eteint         \\
+    AppuyerVert    &    0   &  Init           & Vert           \\
+    AppuyerVert    &    1   &  AppuyerVert    & Vert           \\
+    -------------------------------------------------------------
 */
 /*
-* ========== I/O IDENTIFICATION (CONNEXIONS SUR LE ROBOT) =============
-* 
-*
-* ┌───────────────────────────────────────────────────────────────────┐
-* │                    ATmega324PA - Pinout                           │
-* ├───────────────────────────────────────────────────────────────────┤
-* │                                                                   │
-* │   PORTA            PORTB           PORTC          PORTD           │
-* │  ┌───────┐        ┌──────┐         ┌──────┐       ┌──────┐        │   
-* │  │ PA0 ●─│→B Lum  │PB0 ○ │         │PC0 ○ │       │PD0 ○ │        │
-* │  │ PA1 ●─│→B Lum  │PB1 ○ │         │PC1 ○ │       │PD1 ○ │        │
-* │  │ PA2 ○ │        │PB2 ○ │         │PC2 ○ │       │PD2 ●─│→B-INT0 │
-* │  │ PA3 ○ │        │PB3 ○ │         │PC3 ○ │       │PD3 ●─│→B-INT0 │
-* │  │ PA4 ○ │        │PB4 ○ │         │PC4 ○ │       │PD4 ○ │        │
-* │  │ PA5 ○ │        │PB5 ○ │         │PC5 ○ │       │PD5 ○ │        │
-* │  │ PA6 ○ │        │PB6 ○ │         │PC6 ○ │       │PD6 ○ │        │
-* │  │ PA7 ○ │        │PB7 ○ │         │PC7 ○ │       │PD7 ○ │        │
-* │  └───────┘        └──────┘         └──────┘       └──────┘        │
-* │                                                                   │
-* │  ● = Utilisé          ○ = Non utilisé                             │
-* └───────────────────────────────────────────────────────────────────┘
-* LEGENDE :
-* B Lum  : lumière
-* B-INT0 : bouton
-* =====================================================================
-*/
-
-#include <avr/io.h>
+ * ========== I/O IDENTIFICATION (CONNEXIONS SUR LE ROBOT) =============
+ *
+ *
+ * ┌───────────────────────────────────────────────────────────────────┐
+ * │                    ATmega324PA - Pinout                           │
+ * ├───────────────────────────────────────────────────────────────────┤
+ * │                                                                   │
+ * │   PORTA            PORTB           PORTC          PORTD           │
+ * │  ┌───────┐        ┌──────┐         ┌──────┐       ┌──────┐        │
+ * │  │ PA0 ●─│→B Lum  │PB0 ○ │         │PC0 ○ │       │PD0 ○ │        │
+ * │  │ PA1 ●─│→B Lum  │PB1 ○ │         │PC1 ○ │       │PD1 ○ │        │
+ * │  │ PA2 ○ │        │PB2 ○ │         │PC2 ○ │       │PD2 ●─│→B-INT0 │
+ * │  │ PA3 ○ │        │PB3 ○ │         │PC3 ○ │       │PD3 ●─│→B-INT0 │
+ * │  │ PA4 ○ │        │PB4 ○ │         │PC4 ○ │       │PD4 ○ │        │
+ * │  │ PA5 ○ │        │PB5 ○ │         │PC5 ○ │       │PD5 ○ │        │
+ * │  │ PA6 ○ │        │PB6 ○ │         │PC6 ○ │       │PD6 ○ │        │
+ * │  │ PA7 ○ │        │PB7 ○ │         │PC7 ○ │       │PD7 ○ │        │
+ * │  └───────┘        └──────┘         └──────┘       └──────┘        │
+ * │                                                                   │
+ * │  ● = Utilisé          ○ = Non utilisé                             │
+ * └───────────────────────────────────────────────────────────────────┘
+ * LEGENDE :
+ * B Lum  : lumière
+ * B-INT0 : bouton
+ * =====================================================================
+ */
 #define F_CPU 8000000
+#include <avr/io.h>
 #include <util/delay.h>
-const int debounceDelay = 50;      // Délai de rebond en millisecondes
-const int DelaiAffichageAmbre = 5; // Délai d'affichage pour la couleur ambre en millisecondes
-const int Vert = 0b00000001;
-const int Rouge = 0b00000010;
-const int Eteint = 0b00000000;
+const uint8_t debounceDelay = 50;      // ms
+const uint8_t DelaiAffichageAmbre = 5; // ms
+const uint8_t Rouge = (1 << PA0);
+const uint8_t Vert = (1 << PA1);
+const uint8_t Eteint = 0x00;
 
 enum Etat
 {
@@ -71,27 +70,34 @@ bool BoutonPoussoirAppuyer()
 {
     if (PIND & (1 << PD2))
     {
-        _delay_ms(debounceDelay); // Attendre pour le rebond
-        if (PIND & (1 << PD2))
-        {
-            return true; // Le bouton est appuyé
-        }
+        _delay_ms(debounceDelay);
+        return (PIND & (1 << PD2));
     }
-    else
-    {
-        return false; // Le bouton n'est pas appuyé
-    }
-    // Vérifier si le bouton poussoir est appuyé (niveau bas)
-    return (PIND & (1 << PD2));
+    return false;
 }
-void AllumerLumiere(int couleur)
+void AllumerLumiere(uint8_t couleur)
 {
-    PORTA = couleur; // Allumer la lumière avec la couleur spécifiée
+    switch (couleur)
+    {
+    case Rouge:
+        PORTA |= (1 << PA0);
+        PORTA &= ~(1 << PA1);
+
+        break;
+    case Vert:
+        PORTA |= (1 << PA1);
+        PORTA &= ~(1 << PA0);
+        break;
+    case Eteint:
+        PORTA &= ~(1 << PA0 | 1 << PA1);
+        break;
+
+    default:
+        break;
+    }
 }
 void AfficherAmbre()
 {
-    // Afficher la couleur ambre en alternant rouge et vert rapidement
-    
     AllumerLumiere(Rouge);
     _delay_ms(DelaiAffichageAmbre);
     AllumerLumiere(Vert);
@@ -99,11 +105,8 @@ void AfficherAmbre()
 }
 int main()
 {
-    // Les 4 ports sont en mode sortie
-    DDRA = 0xFF;         // 11111111
-    DDRB = 0xFF;         // 11111111
-    DDRC = 0xFF;         // 11111111
-    DDRD &= ~(1 << PD2); // 11111011
+    DDRA |= (1 << PA0) | (1 << PA1);
+    DDRD &= ~(1 << PD2);
 
     Etat etat = Init;
     while (true)
